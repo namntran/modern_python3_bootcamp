26eee01422098804aa489245b2564221228fd8b9
namntran <namtran78@gmail.com>
2020-01-06 21:15:57 +1000

    input function

---
+++ BooleanandConditionalLogic/input.py
@@ -0,0 +1,7 @@
+# data = input("what is your favourite colour?")
+# print("You said " + data)
+
+#separate print line
+print("what's your favourite colour?")
+data = input()
+print("you said " + data)
