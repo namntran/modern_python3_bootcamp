83274865c267afb219ab6ced25e155edc2f4699b
namntran <namtran78@gmail.com>
2020-01-13 21:29:14 +1000

    conditional logic based on colours

---
+++ BooleanandConditionalLogic/favouriteColour.py
@@ -0,0 +1,11 @@
+colour = input ("What's your favourite colour")
+if colour == 'green':
+    print("excellent choice!")
+elif colour == 'khaki':
+    print("not bad")
+elif colour == 'lime':
+    print("that's ok I guess")
+elif colour == 'emerald':
+    print("that's obscure")
+else:
+    print("weirdo!")
