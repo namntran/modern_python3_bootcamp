8fab2132f800b8780f634f00602eb12ecb0d8a35
namntran <namtran78@gmail.com>
2020-01-20 09:59:14 +1000

    program using for and while loop - ugly solution

--- Loops/smileyface.py
+++ Loops/smileyface.py
@@ -1,10 +1,19 @@
 # print smiley face art
 
-for x in range(3):
-    for num in range(1,11): #nested loop
-        print("\U0001f600" * num)
+# for x in range(3):
+#     for num in range(1,11): #nested loop
+#         print("\U0001f600" * num)
 
 # times = 1
 # while times < 11:
 #     print("\U0001f600" * times)
 #     times += 1
+
+# without string multiplication - ugly solution
+for num in range(1,11): #nested loop
+    count = 1
+    smileys = ""
+    while count < num:
+        smileys += "\U0001f600"
+        count += 1
+    print(smileys)
