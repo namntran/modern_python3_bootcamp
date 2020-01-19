976b2bc609db4d890761cab3fc407b736444a3e7
namntran <namtran78@gmail.com>
2020-01-20 09:18:06 +1000

    convert 'for' loop to a 'while' loop

--- Loops/while.py
+++ Loops/while.py
@@ -1,5 +1,13 @@
-msg = input("what's the secret password? ")
-while msg != "bananas":
-    print("guess again")
-    msg = input("what's the secret password? ")
-print("that's correct!")
+# msg = input("what's the secret password? ")
+# while msg != "bananas":
+#     print("guess again")
+#     msg = input("what's the secret password? ")
+# print("that's correct!")
+
+# for num in range(1,11):
+#     print(num)
+
+num = 1
+while num <= 10:
+    print(num)
+    num += 1
