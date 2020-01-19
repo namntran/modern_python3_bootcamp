24cda181154e17fb24983de05acff62fd51cf143
namntran <namtran78@gmail.com>
2020-01-19 15:59:44 +1000

    refactor numbers program to use only 1 print statement to remove duplication

--- Loops/LuckyNumbers.py
+++ Loops/LuckyNumbers.py
@@ -1,12 +1,16 @@
 #program prints unlucky numbers
+#refactor program to have only one print statement for less duplication
 for n in range(1,21):
     if n == 4 or n == 13:
-        print(f"{n} is unlucky")
+        # print(f"{n} is unlucky")
+        state = "unlucky"
     elif n % 2 == 0:
-         print(f"{n} is even")
+        # print(f"{n} is even")
+        state = "even"
     else:
-        print(f"{n} is odd")
-
+        # print(f"{n} is odd")
+        state = "odd"
+    print(f"{n} is {state}")
    
 
    
