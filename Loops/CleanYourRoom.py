b636bfa3799cab127981d6626023f5909fe0f189
namntran <namtran78@gmail.com>
2020-01-19 13:38:24 +1000

    add program that repeats for loop

---
+++ Loops/CleanYourRoom.py
@@ -0,0 +1,8 @@
+#program that repeats
+times = input ("how many times to I have to tell you? ")
+times = int(times) #convert string into integer
+
+for time in range(times):
+     print("Clean up your room!")
+else: 
+    print("something went wrong")
