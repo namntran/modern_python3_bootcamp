5df1117e503e2564d6697da7a38599ba6cff7223
namntran <namtran78@gmail.com>
2020-01-28 21:01:53 +1000

    guessing game program

--- Loops/1loops.py
+++ Loops/1loops.py
@@ -1,4 +1,21 @@
-i = 1
-while i < 5:
-  i += i #increment i by 1
-  print(i)
+# i = 1
+# while i < 5:
+#   i += i #increment i by 1
+#   print(i)
+
+# print 1 to 5
+# i = 0
+# while i <= 5:
+#   i += 1
+#   print(i)
+
+
+#   from random import randint  # use randint(a, b) to generate a random number between a and b
+from random import randint
+number = 0 #store random number in here, each time through
+i = 0  # i should be incremented by one each iteration
+ 
+while number != 5: #keep looping while number is not 5
+    i += 1
+    number = randint(1, 10) #update number to be a new random int from 1-10
+    print(number)
