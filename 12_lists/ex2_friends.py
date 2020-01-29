144385a53092ec682b84b2f4a51f7caf49ff2384
namntran <namtran78@gmail.com>
2020-01-30 09:51:25 +1000

    access values in a list using for loop

--- 12_lists/ex2_friends.py
+++ 12_lists/ex2_friends.py
@@ -1,2 +1,11 @@
 friends = ["Nikki", "Pebbles", "Vivienne", "Megan"]
 print(friends[0], friends[1],friends[2], friends[3])
+print(friends[-3], friends[-2],friends[-1], friends[0])
+
+if "Nikki" in friends: #True
+    print("True")
+   
+if "Nam" in friends: # False
+    print("True")
+else:
+    print("False")
---
+++ 12_lists/ex3_accessListLoops.py
@@ -0,0 +1,10 @@
+numbers = [1,2,3,4]
+
+for num in numbers:
+    print(num)
+    print(num*num)
+
+colours = ["red", "yellow", "purple", 6.7] #store values in list
+
+for colour in colours:
+    print(colour)
