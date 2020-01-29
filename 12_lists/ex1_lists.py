acd88ede6e99245521f2d71f06f5244cdb8b4bed
namntran <namtran78@gmail.com>
2020-01-30 09:09:19 +1000

    exercises with lists

---
+++ 12_lists/ex1_lists.py
@@ -0,0 +1,6 @@
+# Define my_stuff using a list
+my_stuff = ["mac", "cheese", 3.5, "pasta"]
+print(my_stuff)
+# Define nums 
+nums = list(range(1,11))
+print(nums)
---
+++ 12_lists/ex2_friends.py
@@ -0,0 +1,2 @@
+friends = ["Nikki", "Pebbles", "Vivienne", "Megan"]
+print(friends[0], friends[1],friends[2], friends[3])
