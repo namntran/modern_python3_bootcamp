79cce8e2822ea00fa5542db1e30c00151714c86c
namntran <namtran78@gmail.com>
2020-02-11 21:26:36 +1000

    list comprehension with else logic

--- 12_lists/comprehensionList.py
+++ 12_lists/comprehensionList.py
@@ -2,8 +2,10 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9 , 10]
 evens = [num for num in numbers if num % 2 == 0]
 odds = [num for num in numbers if num % 2 != 0]
+else_logic = [num*2 if num % 2 == 0 else num/2 for num in numbers]
 print(evens)
 print(odds)
+print(else_logic)
 
 #list comprehension convert list of integers to strings
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9 , 10]
