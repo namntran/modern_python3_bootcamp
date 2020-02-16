6b273112b4bd324b95d0dc148c46f605c792c167
namntran <namtran78@gmail.com>
2020-02-16 14:53:16 +1000

    add list comprehension exercises

--- 12_lists/comprehensionList.py
+++ 12_lists/comprehensionList.py
@@ -1,3 +1,16 @@
+# create a variable called answer, which contains a list with all the numbers divisible by 12, for numbers between 1 and 100 (inclusive)
+answer = [val for val in range(1,101) if val % 12 == 0]
+print(answer) 
+
+
+#list comprehension  - given 2 lists, create a variable which is a new list that is the intersection fo the two
+
+answer = [val for val in [1,2,3,4] if val in [3,4,5,6]]
+#the slice [::-1] is a quick way to reverse a string
+answer2 = [val[::-1].lower() for val in ["Elie", "Tim", "Matt"]]
+print(answer)
+print(answer2)
+
 # exercise using list comprehensions:
 
 answer = [person[0] for person in ["Elie", "Tim", "Matt"]]
